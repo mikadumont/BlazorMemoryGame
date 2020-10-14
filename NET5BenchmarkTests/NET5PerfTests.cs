19e7ee6245c4275e00cd78eef41c97e7cfde43a6
Kendra Havens <kehavens@microsoft.com>
2020-10-13 17:05:00 -0700

    Add Benchmark and anlayzers demo

---
+++ NET5BenchmarkTests/NET5PerfTests.cs
@@ -0,0 +1,69 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+[TestClass]
+public class PerfTests1
+{
+    [TestMethod]
+    public void ToUpperPerfTest()
+    {
+        string convert = new HttpClient().GetStringAsync("http://www.gutenberg.org/cache/epub/1112/pg1112.txt").Result;
+
+        for (int i = 0; i < 5000; i++)
+        {
+            convert.ToLower();
+            convert.ToUpper();
+        }
+    }
+
+    private ImmutableList<int> _list = ImmutableList.Create(Enumerable.Range(0, 1_000).ToArray());
+    [TestMethod]
+    public void SumPerfTest()
+    {
+        int sum = 0;
+
+        for (int i = 0; i < 1_000; i++)
+            if (_list.Contains(i))
+                sum += i;
+    }
+
+    [TestMethod]
+    public void RegexPerfTest()
+    {
+        string _input = new HttpClient().GetStringAsync("http://www.gutenberg.org/cache/epub/1112/pg1112.txt").Result;
+        new Regex(@"^.*\blove\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\bRomeo\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\bJuliet\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\blove\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\bRomeo\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\bJuliet\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\blove\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\bRomeo\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\bJuliet\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\blove\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\bRomeo\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+        new Regex(@"^.*\bJuliet\b.*$", RegexOptions.Multiline | (RegexOptions.None));
+    }
+
+    [TestMethod]
+    public void JsonSerializationPerfTest()
+    {
+        MemoryStream _stream = new MemoryStream();
+        DateTime[] _array = Enumerable.Range(0, 1000).Select(_ => DateTime.UtcNow).ToArray();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            _stream.Position = 0;
+            JsonSerializer.SerializeAsync(_stream, _array);
+        }
+
+    }
+}
+
